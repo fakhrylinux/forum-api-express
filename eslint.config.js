d8713dd2ba564d93520e60f4d61c4c9d15b820a2
Fakhry <fakhry.linux@gmail.com>
2026-03-28 21:44:27 +0800

    Membuat Custom Error

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
 import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import vitest from "@vitest/eslint-plugin";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
+    plugins: { js, vitest },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: { ...vitest.environments.env.globals } },
   },
   eslintConfigPrettier,
 ]);
